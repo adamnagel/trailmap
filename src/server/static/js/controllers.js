1452049537965675eaaddf0553f04f837124b5ee
Adam Nagel <adam.nagel+git@gmail.com>
2016-04-04 23:22:03 -0500

    Use ng-material for UI

--- src/server/static/js/controllers.js
+++ src/server/static/js/controllers.js
@@ -2,22 +2,26 @@
 
 /* Controllers */
 
-var trailmapApp = angular.module('trailmapApp', ['ui.bootstrap']);
+var trailmapApp = angular.module('trailmapApp', ['ngMaterial']);
 
 trailmapApp.controller('TrailmapCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.trailsystems = {};
 
     $http.get('api/list_trailsystems').success(function (data) {
-        var trailsystems = data['trailsystems']
+        var trailsystems = data['trailsystems'];
 
         angular.forEach(trailsystems, function (trailsystem) {
             $scope.trailsystems[trailsystem] = {};
         });
 
         angular.forEach(trailsystems, function (trailsystem) {
-            $http.get('api/traildata/' + trailsystem).success(function (traildata) {
+            $http.get('api/' + trailsystem + '/data').success(function (traildata) {
                 $scope.trailsystems[trailsystem] = traildata;
             });
         });
+
+        $scope.activeTrailsystem = 'gsmnp';
+        $scope.startPoint = 'Cosby Campground';
+        $scope.destinationPoint = '34';
     });
 }]);
